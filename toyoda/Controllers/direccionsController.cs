c47d8d54e827947260ee2e857965eccd3c64cbb3
Ronald <ragcorp.g@outlook.com>
2023-06-18 20:55:30 -0600

    Agregar archivos de proyecto.

---
+++ toyoda/Controllers/direccionsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using toyoda.Models;
+
+namespace toyoda.Controllers
+{
+    public class direccionsController : Controller
+    {
+        private TOYYODABDEntities db = new TOYYODABDEntities();
+
+        // GET: direccions
+        public ActionResult Index()
+        {
+            return View(db.direccion.ToList());
+        }
+
+        // GET: direccions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            direccion direccion = db.direccion.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // GET: direccions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: direccions/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idDireccion,idPais,idCiudad,idPersona,direccion1")] direccion direccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.direccion.Add(direccion);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(direccion);
+        }
+
+        // GET: direccions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            direccion direccion = db.direccion.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // POST: direccions/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idDireccion,idPais,idCiudad,idPersona,direccion1")] direccion direccion)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(direccion).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(direccion);
+        }
+
+        // GET: direccions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            direccion direccion = db.direccion.Find(id);
+            if (direccion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(direccion);
+        }
+
+        // POST: direccions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            direccion direccion = db.direccion.Find(id);
+            db.direccion.Remove(direccion);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ toyoda/Controllers/sucursalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using toyoda.Models;
+
+namespace toyoda.Controllers
+{
+    public class sucursalsController : Controller
+    {
+        private TOYYODABDEntities db = new TOYYODABDEntities();
+
+        // GET: sucursals
+        public ActionResult Index()
+        {
+            return View(db.sucursal.ToList());
+        }
+
+        // GET: sucursals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sucursal sucursal = db.sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // GET: sucursals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: sucursals/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idSucursal,idPais,idCiudad,nombreSucursal,idCliente")] sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sucursal.Add(sucursal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sucursal);
+        }
+
+        // GET: sucursals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sucursal sucursal = db.sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: sucursals/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idSucursal,idPais,idCiudad,nombreSucursal,idCliente")] sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sucursal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sucursal);
+        }
+
+        // GET: sucursals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sucursal sucursal = db.sucursal.Find(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: sucursals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            sucursal sucursal = db.sucursal.Find(id);
+            db.sucursal.Remove(sucursal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
