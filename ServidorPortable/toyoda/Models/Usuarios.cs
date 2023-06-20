9334aaccb2b2cd1b0519bad94e99f59693fe1358
Ronald <ragcorp.g@outlook.com>
2023-06-19 20:41:52 -0600

    servidor portable

---
+++ ServidorPortable/toyoda/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+ 
+namespace toyoda.Models
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+
+        public string ConfirmarClave { get; set; }
+
+    }
+}
